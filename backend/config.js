d423ba8ec074f67e5b0d3a7bfe253afda0c22484
naveen <naveenspec@gmail.com>
2024-10-22 22:46:33 -0600

    completed index, show, create routes

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+"mongodb+srv://admin:imAhvmy6CvnCzGx7@cluster0.yejozf3.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0";
